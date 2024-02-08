8a4fba99194534775d28ba55359048fbc2587847
Harish1 <harish1@gmail.com>
2024-02-08 17:41:23 +0530

    Handle Error by adding a middleware "next" and  a function

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,11 +1,13 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     // console.log(req.body);
     const {username, email, password} = req.body;
     if(!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({message: 'All fields are required.'});
+        // return res.status(400).json({message: 'All fields are required.'});
+        next(errorHandler(400, "All fields are required..."));
     }
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({
@@ -18,6 +20,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json({message: 'Signup Successful'});
     } catch(error) {
-        res.status(500).json({message: error.message});
+        // res.status(500).json({message: error.message});
+        next(error);
     }
 }
