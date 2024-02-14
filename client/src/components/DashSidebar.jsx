796f93e3ac87da483001015710cca0fd595f2f87
Harish1 <harish1@gmail.com>
2024-02-14 22:51:18 +0530

    Sidebar Dashboard Completed

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,33 @@
+import React,{useEffect, useState} from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import {Sidebar} from 'flowbite-react';
+import {HiUser, HiArrowSmRight} from 'react-icons/hi'
+
+export default function DashSidebar() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get('tab');
+    // console.log(tabFormUrl);
+    if(tabFormUrl){
+      setTab(tabFormUrl);
+    }
+  },[location.search]);
+  return (
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item active={tab==='profile'} icon={HiUser} label={'User'} labelColor='dark'>
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+            Sign out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  )
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get('tab');
+    // console.log(tabFormUrl);
+    if(tabFormUrl){
+      setTab(tabFormUrl);
+    }
+  },[location.search]);
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar/>
+      </div>
+      {/* Profile */}
+      {tab === 'profile' && <DashProfile/>}
+    </div>
   )
 }
