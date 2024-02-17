f6e3b5144c483179973199627813cda7cdd5256b
Harish1 <harish1@gmail.com>
2024-02-17 17:00:18 +0530

    Complete CreatePost UI Page with Quill (Texteditor)

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,9 +8,10 @@ import 'react-circular-progressbar/dist/styles.css';
 import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signOutSuccess} from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux'
 import {HiOutlineExclamationCircle} from 'react-icons/hi';
+import {Link} from 'react-router-dom';
 
 export default function DashProfile() {
-  const {currentUser, error} = useSelector(state => state.user);
+  const {currentUser, error, loading} = useSelector(state => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -179,7 +180,14 @@ export default function DashProfile() {
         <TextInput type='text' id='username' placeholder='Username' defaultValue={currentUser.username} onChange={handleChange}  />
         <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email} onChange={handleChange} />
         <TextInput type='password' id='password' placeholder='Password' onChange={handleChange}/>
-        <Button type='submit' gradientDuoTone='redToYellow' outline>Update</Button>
+        <Button type='submit' gradientDuoTone='redToYellow' outline disabled={loading || imageFileUploading}>
+          {loading ? 'Loading...' : 'Update'}
+        </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button type='button' gradientDuoTone="purpleToPink" className='w-full'>Create a Post</Button>
+          </Link>
+        )}
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
