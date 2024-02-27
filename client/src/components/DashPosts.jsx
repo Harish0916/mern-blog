19c0ea5c396485887d7ab06caa2faa00091a92f2
Harish1 <harish1@gmail.com>
2024-02-27 11:20:30 +0530

    Add Show more functionality  to the post results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom'
 export default function DashPosts() {
   const { currentUser } = useSelector( state => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
+  const [showMore, setShowMore] = useState(true);
+
+  // console.log(userPosts);
 
   useEffect(()=>{
     const fetchPosts = async () => {
@@ -16,6 +18,9 @@ export default function DashPosts() {
         // console.log(data);
         if(res.ok){
           setUserPosts(data.posts);
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -25,6 +30,24 @@ export default function DashPosts() {
       fetchPosts();
     }
   },[currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts(prev => [...prev, ...data.posts]);
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
+      }
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -71,6 +94,9 @@ export default function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+          { showMore && (
+            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>Show more</button>
+          )}
         </>
       ):(<p>You have no posts yet!</p>)}
     </div>
