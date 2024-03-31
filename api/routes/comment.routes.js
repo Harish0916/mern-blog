6b08f5db8fbd4700591c47376b5e03fb524c6cfa
Harish1 <harish1@gmail.com>
2024-03-31 20:22:12 +0530

    Show the Comments of any post with user detail

--- api/routes/comment.routes.js
+++ api/routes/comment.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment } from '../controllers/comment.controllers.js';
+import { createComment, getPostComments } from '../controllers/comment.controllers.js';
 
 const router = express.Router();
 
-router.post('/create',verifyToken, createComment)
+router.post('/create',verifyToken, createComment);
+router.get('/getPostComments/:postId', getPostComments);
 
 export default router;
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signout, getUsers } from '../controllers/user.controllers.js';
+import { test, updateUser, deleteUser, signout, getUsers, getUser } from '../controllers/user.controllers.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -10,5 +10,6 @@ router.put('/update/:userId',verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
