2a249f9e01faac7556a8cdb023a25b08e4c448db
Harish1 <harish1@gmail.com>
2024-04-05 15:38:41 +0530

    Added Dashboard to the Admin Panel

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import React,{useEffect, useState} from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import {Sidebar} from 'flowbite-react';
-import {HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiAnnotation} from 'react-icons/hi'
+import {HiUser, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiAnnotation, HiChartPie} from 'react-icons/hi'
 import { signOutSuccess } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -37,6 +37,17 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          {currentUser && currentUser.isAdmin && (
+              <Link to='/dashboard?tab=dash'>
+                <Sidebar.Item
+                  active={tab === 'dash' || !tab}
+                  icon={HiChartPie}
+                  as='div'
+                >
+                  Dashboard
+                </Sidebar.Item>
+              </Link>
+          )}
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item 
               active={tab==='profile'} 
