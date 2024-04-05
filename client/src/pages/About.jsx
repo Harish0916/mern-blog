5f37d9a0607d94ee11d87c7ab8a8b56ba4fcb1f9
Harish1 <harish1@gmail.com>
2024-04-05 18:29:02 +0530

    Complete Aboutus and Project page.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7 '>
+            About Harish(Codingoes)' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6 border-b-2 border-t-2 p-2'>
+            <p>
+              Welcome to Codingoes! This blog was created by Harish Ojha
+              as a complete computer programming language & projects to share his thoughts and ideas with the
+              world. Harish is a passionate mentor who believe to write about
+              technology, coding, and everything in between.
+            </p>
+
+            <p>
+              Coming to this blog, you'll find weekly articles and tutorials on topics
+              such as web development, software engineering, and programming
+              languages. So that all engineering student clear their dought. Harish is always learning and exploring new technologies, so be sure to check back often for new content!
+            </p>
+
+            <p>
+              We are very excited to encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. Your carrier is most important to complete their task to acheive your goal.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
